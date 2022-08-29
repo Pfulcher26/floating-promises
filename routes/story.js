49bb202f83e613b2700d6d1e2490826b58090e41
pfulcher26 <pfulcher26@gmail.com>
2022-08-29 16:47:09 -0700

    Adds a route for the creatoin of a comment.

--- routes/story.js
+++ routes/story.js
@@ -6,5 +6,6 @@ router.get('/', storyCtrl.show);
 router.get('/new', storyCtrl.newStory);
 router.post('/', storyCtrl.createStory);
 router.get('/:id', storyCtrl.commentDisplay);
+router.post('/', storyCtrl.commentCreate);
 
 module.exports = router;
