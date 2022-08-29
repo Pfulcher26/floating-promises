86ab0f43fa9c7e30fbd9dd56fc8091624b719f0f
pfulcher26 <pfulcher26@gmail.com>
2022-08-29 10:28:17 -0700

    Creates a model for stories, adds the stories and comments schema, embeds the comments schema inside of the stories schema.

---
+++ models/stories.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const storySchema = new Schema({
+    story: String,
+    name: String, 
+    age: Number, 
+    location: String,
+    date: {
+        type: Number,
+        default: function () {
+            return new Date().getFullYear();
+        }
+    }, 
+    time: {  type: String,
+        enum: ['1 month', 'six months', 'one year', 'year plus']},
+    comments: [commentSchema],
+    userName: String,
+    userAvatar: String,
+    user: {type: Schema.Types.ObjectId, ref: 'User'}
+}, {
+    timestamps: true
+});
+
+const commentSchema = new Schema({
+    comment: String, 
+    name: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Number,
+        default: function () {
+            return new Date().getFullYear();
+        }
+    },
+    userName: String,
+    userAvatar: String,
+    user: {type: Schema.Types.ObjectId, ref: 'User'},
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Story', storySchema);
