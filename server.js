b33022793fdd85ae4208a63fd41d2d0fe896e3c9
pfulcher26 <pfulcher26@gmail.com>
2022-08-29 11:11:50 -0700

    Requires and uses the about router in the server.

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mainRouter = require('./routes/main');
 var storyRouter = require('./routes/story');
+var aboutRouter = require('./routes/about');
 
 var app = express();
 
@@ -30,6 +31,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/main', mainRouter);
 app.use('/story', storyRouter);
+app.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
