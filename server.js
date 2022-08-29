85077091588ea0e8d5e25903055f662d06b57f1b
pfulcher26 <pfulcher26@gmail.com>
2022-08-29 10:59:26 -0700

    Requires and uses the story route in the server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ require('./config/database');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mainRouter = require('./routes/main');
+var storyRouter = require('./routes/story');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use(methodOverride('_method'));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/main', mainRouter);
+app.use('/story', storyRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
