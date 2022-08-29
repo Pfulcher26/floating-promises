584aa4e240425cc3a20df1f4b01d624ed193c461
pfulcher26 <pfulcher26@gmail.com>
2022-08-29 11:07:29 -0700

    Crates an about directory in the views module.

--- routes/story.js
+++ routes/story.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const mainCtrl = require('../controllers/story');
+const storyCtrl = require('../controllers/story');
 
 router.get('/', storyCtrl.show);
 
