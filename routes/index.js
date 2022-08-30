355066b34a647f8595107ea09e118668cdd9a533
pfulcher26 <pfulcher26@gmail.com>
2022-08-30 14:54:19 -0700

    Adds routes in the index.js route for google Oauth.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'The Weather Tomorrow' });
+});
+
+router.get('/auth/google', passport.authenticate(
+  'google',
+  { scope: ['profile', 'email'] }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect : '/story',
+    failureRedirect : '/'
+  }
+));
+
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/movies');
 });
 
 module.exports = router;
