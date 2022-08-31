105db1053705d5d34605e4b6a79346fcc7499453
pfulcher26 <pfulcher26@gmail.com>
2022-08-31 11:13:39 -0700

    Updates landing page.  Oauth not working.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'The Weather Tomorrow' });
+  res.render('main/index', { title: 'The Weather Tomorrow' });
 });
 
 router.get('/auth/google', passport.authenticate(
